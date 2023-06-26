f1ceec1f92270a9e349405cac7109171c7eb90af
kabirshuvo <kabirshuvo19@gmail.com>
2023-06-26 17:53:53 +0600

    Practicing Next Js GraphQl GraphBase

---
+++ constant/index.ts
@@ -0,0 +1,78 @@
+export const NavLinks = [
+  { href: "/", key: "Grettings", text: "Hi" },
+  { href: "/", key: "Skills", text: "Skills" },
+  { href: "/", key: "Projects", text: "Projects" },
+  { href: "/", key: "Testimonials", text: "Testimonials" },
+  { href: "/", key: "Resume", text: "Resume" },
+];
+
+export const categoryFilters = [
+  "Frontend",
+  "Backend",
+  "Full-Stack",
+  "Mobile",
+  "UI/UX",
+  "Game Dev",
+  "DevOps",
+  "Data Science",
+  "Machine Learning",
+  "Cybersecurity",
+  "Blockchain",
+  "E-commerce",
+  "Chatbots",
+];
+
+export const footerLinks = [
+  {
+    title: "MY Tech Path",
+    links: [
+      "Symantic HTML",
+      "Vanila CSS",
+      "Vanila javaScript",
+      "Development blog",
+      "Open-source projects",
+      "Refer a Friend",
+      "Leave Me a Comment",
+    ],
+  },
+  {
+    title: "Hire Me",
+    links: [
+      "Call me for a interview",
+      "Hire me Hourly ",
+      "Hire me Permanently",
+    ],
+  },
+  {
+    title: "Resume",
+    links: ["Download My Resume"],
+  },
+  {
+    title: "Me-Myself",
+    links: ["About me", "Careers", "Projects", "Hobbies", "Testimonials"],
+  },
+  {
+    title: "My Team",
+    links: ["Advisors", "Developers", "Deginners", "Marketers", "Editors"],
+  },
+  {
+    title: "My Tools",
+    links: [
+      "Front Technologies",
+      "Backend Technologies",
+      "Full-stack Technologies",
+      "Others",
+    ],
+  },
+  {
+    title: "My Tech Journey",
+    links: [
+      "Freelancer",
+      "Developer",
+      "Development Portfolio",
+      "Development Education",
+      "Creative Process",
+      "Development Industry Trends",
+    ],
+  },
+];
